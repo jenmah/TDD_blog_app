81f6311fa13fe7bc7bf36b092792d59240a65fa0
Jennifer Mah <jen.mah@hotmail.com>
2015-06-04 20:28:11 +0100

    wrote test for 'can add a comment to a post'

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
+  root 'posts#index'
+
+  resources :posts do
+    resources :comments
+  end
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
