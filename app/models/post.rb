44f19b35b2feb9ca942df390b9c58cddbaec8dd2
Jennifer Mah <jen.mah@hotmail.com>
2015-06-04 19:45:22 +0100

    test to show multiple posts is passing

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,4 +5,14 @@ class Post < ActiveRecord::Base
   validates :author, presence: true
   validates :content, presence: true
   validates :date, presence: true
+
+  def show_comments
+    return 'No comments' if comments.none?
+    # return self.comments.first if self.comments.all.count < 2
+    self.comments.all
+    # self.comments.each do |comment|
+    #   comment.opinion
+    # end
+  end
+
 end
